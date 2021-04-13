3db8361589a2fd58a5ba4de80ee73fa6dbe0cb43
Sabbir Ahmed <sabbirahmed628544@gmail.com>
2021-04-13 11:24:57 +0600

    Problem solve

---
+++ WaterMelon/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WaterMelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w, result;
+            w = Convert.ToInt32(Console.ReadLine());
+            if (w > 2 && w % 2 == 0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
