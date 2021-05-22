5c59d2cf0878f04781e7476c7bdccf8e041744e6
Sabbir Ahmed <61914131+HasanTarik-REC@users.noreply.github.com>
2021-05-22 15:52:04 +0600

    File Edit

--- A. Wrong Subtraction/Program.cs
+++ A. Wrong Subtraction/Program.cs
@@ -10,21 +10,19 @@ namespace A._Wrong_Subtraction
             string n =input[0];
             int k =int.Parse(input[1]);
             int count = 0;
-            int c = n.Length - 1;
             long e;
 
 
             while (count != k)
             {                
-                if (n[c] == '0')
+                if (n[n.Length-1] == '0')
                 {
                     long num =long.Parse(n);
                     e = num / 10;
                     count++;
                     n = e.ToString();
-                    c--;
                 }
-                 else if (n[c] != '0')
+                 else if (n[n.Length-1] != '0')
                 {
                     long num =long.Parse(n);
                     e = num - 1;
