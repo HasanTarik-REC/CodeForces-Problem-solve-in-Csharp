a68458a2652be077214db2ebe430c2bb7aa747a4
Sabbir Ahmed <61914131+HasanTarik-REC@users.noreply.github.com>
2021-05-22 17:47:31 +0600

    Problem solve

---
+++ A. Word/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace A._Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int count = 0;
+            int p = 0;
+            for(int i = 0; i < input.Length; i++)
+            {
+                for (int j = 65;j<= 90; j++)
+                {
+                    if (input[i] == j)
+                        count++;
+                }
+                for(int k = 97; k <= 122; k++)
+                {
+                    if (input[i] == k)
+                    {
+                        p++;
+                    }
+                }
+            }
+            if (count == p)
+            {
+                input = input.ToLower();
+            }
+            else if (count > p)
+            {
+                input = input.ToUpper();
+            }
+            else if (p > count)
+            {
+                input = input.ToLower();
+            }
+            Console.WriteLine(input);
+        }
+    }
+}
