7fd623895b0de2b78dd0a4fdeb3d2a51a32cd34e
Sabbir Ahmed <61914131+HasanTarik-REC@users.noreply.github.com>
2021-05-22 15:45:04 +0600

    A. Wrong Subtraction solve

---
+++ A. Wrong Subtraction/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace A._Wrong_Subtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            string n =input[0];
+            int k =int.Parse(input[1]);
+            int count = 0;
+            int c = n.Length - 1;
+            long e;
+
+
+            while (count != k)
+            {                
+                if (n[c] == '0')
+                {
+                    long num =long.Parse(n);
+                    e = num / 10;
+                    count++;
+                    n = e.ToString();
+                    c--;
+                }
+                 else if (n[c] != '0')
+                {
+                    long num =long.Parse(n);
+                    e = num - 1;
+                    count++;
+                    n = e.ToString();
+                } 
+            }
+            Console.WriteLine(n);   
+        }
+    }
+}
