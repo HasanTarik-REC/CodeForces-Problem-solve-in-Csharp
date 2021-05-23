f6adfba24a5ae56bcbc6c8bbc4db17464e5d7ee3
Sabbir Ahmed <61914131+HasanTarik-REC@users.noreply.github.com>
2021-05-23 14:43:46 +0600

    Problem solve

---
+++ A. Word Capitalization/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace A._Word_Capitalization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = Console.ReadLine();
+            char text = input1[0];
+            string input = input1.Substring(1);
+            string n = text.ToString();
+            n=n.ToUpper();
+            n = n + input;
+            Console.WriteLine(n);
+        }
+    }
+}
