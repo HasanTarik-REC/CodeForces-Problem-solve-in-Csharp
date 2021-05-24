6ccc09b03c4cf12fcb0d4327b3b840df93e33c37
Sabbir Ahmed <61914131+HasanTarik-REC@users.noreply.github.com>
2021-05-24 18:16:18 +0600

    Problem solve

---
+++ A. Anton and Danik/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace A._Anton_and_Danik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count1 = 0, count2 = 0;
+            int n = int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            char[] nn = new char[n];
+            nn = s.ToCharArray();
+            for(int i = 0; i < n; i++)
+            {
+                if (nn[i] == 'A')
+                {
+                    count1++;
+                }
+                else
+                {
+                    count2++;
+                }
+            }
+            if(count1>count2)
+                Console.WriteLine("Anton");
+            else if(count2>count1)
+                Console.WriteLine("Danik");
+            else
+                Console.WriteLine("Friendship");
+
+        }
+    }
+}
