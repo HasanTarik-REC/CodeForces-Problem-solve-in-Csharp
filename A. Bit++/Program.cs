da180784573b79d79396adda3323949242fd5c20
Sabbir Ahmed <61914131+HasanTarik-REC@users.noreply.github.com>
2021-05-24 16:25:45 +0600

    Problem solve

---
+++ A. Bit++/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace A._Bit__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            int x = 0;
+            var input = Console.ReadLine();
+            int n = int.Parse(input);
+            char[] name = new char[3];
+            while (count != n)
+            {
+                var p=(Console.ReadLine());
+                name=p.ToCharArray();
+                if (name[0] == 'x')
+                {
+                    if (name[1] == '+')
+                    {
+                        x++;
+                    }
+                    else if (name[1] == '-')
+                    {
+                        x--;
+                    }
+                }
+                else
+                {
+                    if (name[0] == '+')
+                    {
+                        ++x;
+                    }
+                    else if (name[0] == '-')
+                    {
+                        --x;
+                    }
+                }
+                count++;
+            }
+            Console.WriteLine(x);
+        }
+    }
+}
